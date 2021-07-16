e75ef724cdcfddd20514e6c6e02f18aa4355c405
KuzVlad <kuzvlad1993@gmail.com>
2021-07-16 16:51:39 +0300

    fixed rf627_old_reboot_device_request_to_scanner

--- src/rf62X_sdk.c
+++ src/rf62X_sdk.c
@@ -1731,8 +1731,7 @@ uint8_t send_reboot_device_request_to_scanner(scanner_base_t *device, protocol_t
     case kRF627_OLD:
         switch (protocol) {
         case kSERVICE:
-            rf627_old_reboot_device_request_to_scanner(device->rf627_old);
-            return FALSE;
+            return rf627_old_reboot_device_request_to_scanner(device->rf627_old);
             break;
         case kETHERNET_IP:
         case kMODBUS_TCP:
